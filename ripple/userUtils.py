85f504e51b2aa6bf6b5e8b44bf39d64bd7d74205
Nyo <to@nyo.zz.mu>
2016-12-26 09:56:38 +0100

    Removed some schiavo logs

--- ripple/userUtils.py
+++ ripple/userUtils.py
@@ -985,7 +985,7 @@ def verifyUser(userID, hashes):
 	else:
 		# No matches found, set USER_PUBLIC and USER_NORMAL flags and reset USER_PENDING_VERIFICATION flag
 		resetPendingFlag(userID)
-		log.info("User **{}** ({}) has verified his account with hash set _{}_".format(username, userID, hashes[2:5]), "cm")
+		#log.info("User **{}** ({}) has verified his account with hash set _{}_".format(username, userID, hashes[2:5]), "cm")
 
 		# Allow login
 		return True
