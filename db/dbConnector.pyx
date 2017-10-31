faf526eb527d0667922ba42f965a8506c18c5fd1
Giuseppe Guerra <to@nyo.zz.mu>
2017-10-31 12:01:00 +0100

    Force utf-8 in MySQL driver

--- db/dbConnector.pyx
+++ db/dbConnector.pyx
@@ -69,7 +69,15 @@ class connectionsPool:
 		"""
 		db = MySQLdb.connect(*self.config)
 		db.autocommit(True)
-		#db.cursor(MySQLdb.cursors.DictCursor).execute("SET SESSION query_cache_type = 0;")
+
+		# Force utf-8
+		db.set_character_set("utf8")
+		dbc = db.cursor(MySQLdb.cursors.DictCursor)
+		dbc.execute("SET NAMES utf8;")
+		dbc.execute("SET CHARACTER SET utf8;")
+		dbc.execute("SET character_set_connection=utf8;")
+		dbc.close()
+		
 		conn = worker(db, temporary)
 		return conn
 
